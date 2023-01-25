bacbd6a6d6894e21929d7cc8adb2e6f245ab22e1
tsukhwani <tsukhwani@gantner.com>
2023-01-25 14:25:40 +0530

    Tictactoe - inital commit

---
+++ kata-dotnet-core-start.Lib.Tests/TicTacToeTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace kata_dotnet_core_start.Lib.Tests;
+
+public class TicTacToeTests
+{
+    [Fact] 
+    public void InitialBoard()
+    {
+        //Arrange
+        
+        //Act
+        var result = TicTacToe.GetInitialBoard();
+
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(9,result.Count);
+    }
+}
---
+++ kata-dotnet-core-start.Lib/TicTacToe.cs
@@ -0,0 +1,16 @@
+﻿namespace kata_dotnet_core_start.Lib;
+
+public class TicTacToe
+{
+    public static Dictionary<int, string> board { get; set; } = new Dictionary<int, string>();
+    
+    public static Dictionary<int, string> GetInitialBoard()
+    {
+        for (var i = 0; i < 9; i++)
+        {
+            board.Add(i+1,"");
+        }
+        
+        return board;
+    }
+}
