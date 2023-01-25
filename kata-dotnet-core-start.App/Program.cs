73908a9c25784b74e4370a1f35d2fd6b6d2d2a30
tsukhwani <tsukhwani@gantner.com>
2023-01-25 14:30:58 +0530

    board print

--- kata-dotnet-core-start.App/Program.cs
+++ kata-dotnet-core-start.App/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using kata_dotnet_core_start.Lib;
+
 Console.WriteLine("Hello, World!");
+TicTacToe.Board();
--- kata-dotnet-core-start.Lib/TicTacToe.cs
+++ kata-dotnet-core-start.Lib/TicTacToe.cs
@@ -8,9 +8,23 @@ public class TicTacToe
     {
         for (var i = 0; i < 9; i++)
         {
-            board.Add(i+1,"");
+            board.Add(i+1, (i + 1).ToString());
         }
         
         return board;
+    }
+    
+    public static void Board()
+    {
+        var arr = GetInitialBoard();
+        Console.WriteLine("     |     |      ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+        Console.WriteLine("_____|_____|_____ ");
+        Console.WriteLine("     |     |      ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+        Console.WriteLine("_____|_____|_____ ");
+        Console.WriteLine("     |     |      ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+        Console.WriteLine("     |     |      ");
     }
 }
