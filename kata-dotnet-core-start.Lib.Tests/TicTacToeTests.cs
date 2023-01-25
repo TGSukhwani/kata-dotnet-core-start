418b233dfafc49cd77fdb49403a1ad0c1734bc9b
jvadaliya <jvadaliya@gantner.com>
2023-01-25 14:48:47 +0530

    Check the values of board

--- kata-dotnet-core-start.Lib.Tests/TicTacToeTests.cs
+++ kata-dotnet-core-start.Lib.Tests/TicTacToeTests.cs
@@ -5,7 +5,7 @@ namespace kata_dotnet_core_start.Lib.Tests;
 public class TicTacToeTests
 {
     [Fact] 
-    public void InitialBoard()
+    public void Get_InitialBoard()
     {
         //Arrange
         
@@ -15,5 +15,21 @@ public class TicTacToeTests
         //Assert
         Assert.NotNull(result);
         Assert.Equal(9,result.Count);
+        Assert.False(result.ContainsValue("X"));
+        Assert.False(result.ContainsValue("O"));
     }
+    
+    // [Fact] 
+    // public void Get_GameOverMessage_When_All_Fields_are_taken()
+    // {
+    //     //Arrange
+    //     
+    //     //Act
+    //     var result = TicTacToe.GetInitialBoard();
+    //
+    //     //Assert
+    //     Assert.NotNull(result);
+    //     Assert.Equal(9,result.Count);
+    //     Assert.Equal(9, result.Count(x => x.Value is "X" or "O"));
+    // }
 }
